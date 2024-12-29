8ab249512872362e0eeb5ba6d96ce76a6b1590ae
Ama Samaraweera <samaraweera97427@gmail.com>
2024-12-29 09:39:22 +0530

    Add searchSchedules endpoint to retrieve schedules based on route and date

--- src/controllers/schedule.controller.js
+++ src/controllers/schedule.controller.js
@@ -1,4 +1,5 @@
 import Schedule from "../models/schedule.model.js";
+import Route from "../models/route.model.js"
 
 export const createSchedule = async (req, res) => {
   try {
@@ -11,10 +12,58 @@ export const createSchedule = async (req, res) => {
 
 export const getSchedules = async (req, res) => {
   try {
-    const schedules = await Schedule.find();
+    const schedules = await Schedule.find()
+    .populate("bus_id")
+    .populate("route_id");
     res.status(200).json(schedules);
   } catch (error) {
     res.status(404).json({ message: error.message });
+  }
+};
+
+export const searchSchedules = async (req, res) => {
+  try {
+    const { from, to, date } = req.query;
+
+    if (!from || !to || !date) {
+      return res.status(400).json({ message: "FROM, TO, and DATE are required!" });
+    }
+
+    const routes = await Route.find({
+      start_point: { $regex: new RegExp(from, "i") },
+      end_point: { $regex: new RegExp(to, "i") },
+    });
+
+    if (routes.length === 0) {
+      return res.status(404).json({ message: "No routes found!" });
+    }
+
+    const routeIds = routes.map((route) => route._id);
+
+    const schedules = await Schedule.find({
+      route_id: { $in: routeIds },
+      depature_time: {
+        $gte: new Date(date + "T00:00:00Z"),
+        $lt: new Date(date + "T23:59:59Z"),
+      },
+    })
+      .populate("bus_id")
+      .populate("route_id");
+
+    
+    const results = schedules.map((schedule) => ({
+      start_point: schedule.route_id.start_point,
+      end_point: schedule.route_id.end_point,
+      date_time: schedule.depature_time,
+      bus_type: schedule.route_id.type,
+      bus_schedule_id: schedule._id,
+      available_seats: schedule.available_seats,
+      duration: schedule.route_id.duration,
+    }));
+
+    res.status(200).json(results);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
   }
 };
 
